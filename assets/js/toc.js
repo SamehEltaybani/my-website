079f08f3ab691397032fc0e0f931a0da58d60c06
SamehEltaybani <eltaybanisameh@hotmail.com>
2026-08-22 17:55:09 +0900

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -22,7 +22,7 @@
         contentSelector: '#page-content',
         headingSelector: 'h2, h3',
         excludeClass: 'no-toc',
-        scrollOffset: 180
+        scrollOffset: 120
     };
 
     let tocItems = [];
